9e7acdcb940fdd8a5376eb3fbd6738a68c292a81
Tommaso Bona <piergeolo@gmail.com>
2022-07-13 16:51:04 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -6,11 +6,11 @@ echo "Checking if token is valid..."
 while [ $intertube -ne 1 ]; do
         ping -c 3 https://discord.gift/$token
         if [ $? -eq  0 ]; then
-                echo -e "Valid token :)"
+                echo "Valid token! "
                 say success
                 intertube=1;
         else
-                echo "Invalid token, try again :("
+                echo "Invalid token, try again! "
         fi
 done
 }
