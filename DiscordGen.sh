a9aec53151b67c8879b949683fcb8acbafe70a92
Tommaso Bona <piergeolo@gmail.com>
2022-07-13 16:24:44 +0200

    Update DiscordGen.sh

--- DiscordGen.sh
+++ DiscordGen.sh
@@ -2,10 +2,32 @@ banner(){
 toilet DiscordGen
 }
 menu(){
-
-
-
-
-
+echo -e "$blue             ＞＞＞＞>>>＞$blue[Menu]$blue＜<<<＜＜＜＜$blue"
+echo " "
+echo " "
+echo -e "$blue                        ➡$blue [\e[34m1\e[34m]$blue Launch DiscordGen "
+echo -e "$blue                        ➡$blue [\e[34m2\e[34m]$blue About Me "
+echo -e "$blue                        ➡$blue [\e[34m3\e[34m]$blue Exit "
+echo -ne "\e[34m#Select Option\e[34m: "
+read optnz
+if [ $optnz = "1" ];
+then
+banner
+gen
+elif [ $optnz = "2" ];
+then
+banner
+Android intent action view
+elif [ $optnz = "3" ];
+then
+banner
+echo -ne "Host (www.example.com): "
+read host3
+nmap -sV -T4 -v --script=$PWD/ $host3
+fi
+}
+banner
+menu
+gen(){
 token=$RANDOM | md5sum | head -c 16;
 echo -e "https://discord.gift/$token
