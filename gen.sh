069785688e78b4b0bc2260c6b0188dc76a6bfeea
Tommaso Bona <piergeolo@gmail.com>
2022-07-13 19:30:08 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@ gen(){
 token=$RANDOM | md5sum | head -c 16;
 echo -e "https://discord.gift/$token
 intertube=0
-echo "Checking if token is valid..."
+echo "Checking validity of token..."
 while [ $intertube -ne 1 ]; do
         ping -c 3 https://discord.gift/$token
         if [ $? -eq  0 ]; then
