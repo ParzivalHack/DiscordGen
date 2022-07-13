076a9c6fbf749e04b64fea9656d4e4826dd0ec0b
Tommaso Bona <piergeolo@gmail.com>
2022-07-13 16:37:49 +0200

    Update DiscordGen.sh

--- DiscordGen.sh
+++ DiscordGen.sh
@@ -31,3 +31,15 @@ menu
 gen(){
 token=$RANDOM | md5sum | head -c 16;
 echo -e "https://discord.gift/$token
+intertube=0
+echo "Checking if token is valid..."
+while [ $intertube -ne 1 ]; do
+        ping -c 3 https://discord.gift/$token
+        if [ $? -eq  0 ]; then
+                echo "Valid token :)";
+                say success
+                intertube=1;
+        else
+                echo "Invalid token, try again :("
+        fi
+done
