a48b193c7580a99a0f840c4d4877786aa1b332a8
Tommaso Bona <piergeolo@gmail.com>
2022-07-13 16:46:33 +0200

    Create gen.sh

---
+++ gen.sh
@@ -0,0 +1,16 @@
+gen(){
+token=$RANDOM | md5sum | head -c 16;
+echo -e "https://discord.gift/$token
+intertube=0
+echo "Checking if token is valid..."
+while [ $intertube -ne 1 ]; do
+        ping -c 3 https://discord.gift/$token
+        if [ $? -eq  0 ]; then
+                echo "Valid token :)";
+                say success
+                intertube=1;
+        else
+                echo "Invalid token, try again :("
+        fi
+done
+}
